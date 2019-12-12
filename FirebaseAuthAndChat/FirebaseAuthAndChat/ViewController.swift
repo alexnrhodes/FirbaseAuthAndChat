ca75eb9c0a2606bf86cbc7eb65e2c9bc20ac5454
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-12-12 16:34:09 -0600

    imported Firebase, and completed storyboard setup

--- FirebaseAuthAndChat/FirebaseAuthAndChat/ViewController.swift
+++ FirebaseAuthAndChat/FirebaseAuthAndChat/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
 
-
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        let authUI = FUIAuth.defaultAuthUI()
+        guard authUI != nil else { return }
+        
+        authUI?.delegate = self
+        
+        let authVC = authUI!.authViewController()
+        
+        present(authVC, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: FUIAuthDelegate {
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        if error != nil {
+            return
+        }
+        
+        performSegue(withIdentifier: "LoginSegue", sender: self)
+    }
+}
